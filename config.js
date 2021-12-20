9c72c0b2d7f1ba7cfee2e7d9d2e035670e163892
NNam2k <96414468+NNam2k@users.noreply.github.com>
2021-12-20 16:26:54 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    introTitle: 'Hmmm hi !!!',
+    introDesc: `Khi ma cau bam vao cai link nay nghia la cau cung k co ai de di choi cung dung khumm?? `,
+    btnIntro: 'dung z ',
+    title: 'Di choi cung nn ?? ',
+    desc: 'Di choi cung nn ??  ',
+    btnYes: 'Di chu tr oii <3 ',
+    btnNo: 'Không, ham l a? ',
+    question:'The may h nhi? ',
+    btnReply: ' okiii ',
+    reply: '8h nhe aiu, <3',
+    mess: 'Anh biết mà :3 ',
+    messDesc: 'Tối mai 8h anh qua đón nhé công chúa, d ra t xach cl di :) .',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/ngocnam.pham2009/' 
